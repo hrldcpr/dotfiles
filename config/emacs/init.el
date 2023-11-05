923e47391887cb27bbd73d5e34ce5e87e4bd8c33
Harold Cooper <hrldcpr@gmail.com>
2023-11-05 16:48:55 -0500

    emacs markdown and prettier

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -8,12 +8,14 @@
 (add-hook 'c++-mode-hook (lambda () (add-hook 'before-save-hook 'clang-format-buffer nil 'local)))
 (add-hook 'glsl-mode-hook (lambda () (add-hook 'before-save-hook 'clang-format-buffer nil 'local)))
 
+(add-hook 'markdown-mode-hook 'prettier-js-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(clang-format elixir-mode)))
+ '(package-selected-packages '(prettier-js markdown-mode clang-format elixir-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
