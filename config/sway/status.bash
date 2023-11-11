ef27137319542bd80420dc0bbb84ffebeb00cf7d
Harold Cooper <hrldcpr@gmail.com>
2023-11-11 16:06:47 -0500

    status.bash runs forever

--- config/sway/status.bash
+++ config/sway/status.bash
@@ -1,13 +1,18 @@
 
-# TODO make this a long-running script (in python?) and have volume and brightness buttons (in sway config) send some sort of event to trigger immediate updates
+while true; do
+    # make volume and brightness buttons (in sway config) send some sort of event/signal to trigger immediate updates
+    # (maybe convert this script to python or something for that)
 
-battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-battery_status=$(cat /sys/class/power_supply/BAT0/status)
+    battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+    battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
-brightness=$(bash ~/.config/sway/brightness.bash)
+    brightness=$(bash ~/.config/sway/brightness.bash)
 
-volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -1)
+    volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -1)
 
-date=$(date +'%Y-%m-%d %I:%M %p')
+    date=$(date +'%Y-%m-%d %I:%M %p')
 
-echo Volume: "$volume" Brightness: "$brightness"% Battery: "$battery_capacity"% "$battery_status" "$date"
+    echo sup Volume: "$volume" Brightness: "$brightness"% Battery: "$battery_capacity"% "$battery_status" "$date"
+
+    sleep 60
+done
