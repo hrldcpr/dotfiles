09b30a32d8c1393b4cf4495780becdd7a88b0b2e
Harold Cooper <hrldcpr@gmail.com>
2023-11-11 15:02:21 -0500

    audio and backlight buttons!

---
+++ config/sway/brightness.sh
@@ -0,0 +1,19 @@
+
+max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+step=$((max_brightness / 5)) # 20%
+
+brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+
+if [ "$1" = "down" ]; then
+    min_brightness=$((max_brightness / 100 + 1)) # 1%
+
+    brightness=$((brightness - step))
+    if ((brightness < min_brightness)); then brightness=$min_brightness; fi
+    echo "$brightness" > /sys/class/backlight/intel_backlight/brightness
+elif [ "$1" = "up" ]; then
+    brightness=$((brightness + step))
+    if ((brightness > max_brightness)); then brightness=$max_brightness; fi
+    echo "$brightness" > /sys/class/backlight/intel_backlight/brightness
+fi
+
+echo $((100 * brightness / max_brightness))
