2d76f49347ba4c3556a37a295afefa8aad065dfd
Harold Cooper <hrldcpr@gmail.com>
2023-11-11 17:00:18 -0500

    note

--- config/sway/status.bash
+++ config/sway/status.bash
@@ -2,6 +2,7 @@
 while true; do
     # make volume and brightness buttons (in sway config) send some sort of event/signal to trigger immediate updates
     # (maybe convert this script to python or something for that)
+    # (or just use i3blocks which already does all of that)
 
     battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
     battery_status=$(cat /sys/class/power_supply/BAT0/status | tr A-Z a-z)
