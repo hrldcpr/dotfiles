336d9b0dde56dc1f2014bbcc5e22d118b8aa3c8f
Harold Cooper <hrldcpr@gmail.com>
2023-11-11 16:03:28 -0500

    only update status bar once a minute

--- config/sway/status.bash
+++ config/sway/status.bash
@@ -1,4 +1,6 @@
 
+# TODO make this a long-running script (in python?) and have volume and brightness buttons (in sway config) send some sort of event to trigger immediate updates
+
 battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
@@ -6,6 +8,6 @@ brightness=$(bash ~/.config/sway/brightness.bash)
 
 volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -1)
 
-date=$(date +'%Y-%m-%d %I:%M:%S %p')
+date=$(date +'%Y-%m-%d %I:%M %p')
 
 echo Volume: "$volume" Brightness: "$brightness"% Battery: "$battery_capacity"% "$battery_status" "$date"
