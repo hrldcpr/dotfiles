9ec10efc0a47236b9791d02db2b9162545ec6c09
Harold Cooper <hrldcpr@gmail.com>
2023-11-11 16:20:26 -0500

    status style

--- config/sway/status.bash
+++ config/sway/status.bash
@@ -4,16 +4,16 @@ while true; do
     # (maybe convert this script to python or something for that)
 
     battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-    battery_status=$(cat /sys/class/power_supply/BAT0/status)
+    battery_status=$(cat /sys/class/power_supply/BAT0/status | tr A-Z a-z)
 
     brightness=$(bash ~/.config/sway/brightness.bash)
 
     volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -1)
 
-    date=$(date +'%Y-%m-%d %I:%M %p')
+    date=$(date +'%m/%d %-I:%M%P')
 
-    echo Volume: "$volume" Brightness: "$brightness"% Battery: "$battery_capacity"% "$battery_status" "$date"
+    echo "$volume" volume - "$brightness"% brightness - "$battery_capacity"% "$battery_status" - "$date"
 
     # TODO update time at the _beginning_ of each minute
-    sleep 59
+    sleep 10
 done
