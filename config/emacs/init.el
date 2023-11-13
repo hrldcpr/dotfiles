7083a203b1405f5aa3ba2f19ff4ddd4d5d798d5f
Harold Cooper <hrldcpr@gmail.com>
2023-11-12 19:49:19 -0500

    emacs black python formatter

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -12,12 +12,15 @@
 
 (add-hook 'markdown-mode-hook 'prettier-js-mode)
 
+(add-hook 'python-mode-hook 'blacken-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(prettier-js markdown-mode clang-format elixir-mode)))
+ '(package-selected-packages
+   '(blacken prettier-js markdown-mode clang-format elixir-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
