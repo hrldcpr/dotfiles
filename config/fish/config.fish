d12ab3bfd73e774b68ba257ea941d2f744b81a6c
Harold Cooper <hrldcpr@gmail.com>
2023-11-15 23:51:21 -0500

    Actually, xwayland is fine by me; plus this has no effect on dmenu Revert "firefox does not default to wayland"
    This reverts commit 42bcd0fe9a81c29ffc6fd47b3a2fc59932217ebe.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fzf_key_bindings
     . ~/.asdf/asdf.fish
-    # stop firefox from using xwayland:
-    set --export MOZ_ENABLE_WAYLAND 1
 end
