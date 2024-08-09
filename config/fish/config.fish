fb834f80dbec8d8c77a390ec94f999d58e121b00
Harold Cooper <hrldcpr@gmail.com>
2024-08-09 00:01:54 -0400

    only enable asdf if it's installed (since I don't use it on Linode, but do on Thinkpad)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,5 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     export EDITOR=emacs
     fzf_key_bindings
-    . ~/.asdf/asdf.fish
+    [ -e ~/.asdf/asdf.fish ] && . ~/.asdf/asdf.fish
 end
